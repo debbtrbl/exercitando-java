f129f90363cc8b61c8914294f5aaf4941b9578cb
debb <157556440+debbtrbl@users.noreply.github.com>
2025-07-09 20:24:37 -0300

    Update Principal.java

--- excecoes/src/exercicio/Principal.java
+++ excecoes/src/exercicio/Principal.java
@@ -1,10 +1,47 @@
 package exercicio;
 
+import java.util.Scanner;
+
 public class Principal {
+    public static void main(String[] args) {
+        try {
+            Scanner entrada = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+            System.out.println("Digite o primeiro número: ");
+            int num1 = entrada.nextInt();
 
-	}
+            System.out.println("Digite o segundo número: ");
+            int num2 = entrada.nextInt();
 
+            Calculadora cal = new Calculadora(num1, num2);
+
+            System.out.println("Escolha uma operação: ");
+            System.out.println("Adição [+] ");
+            System.out.println("Subtração [-] ");
+            System.out.println("Multiplicação [*] ");
+            System.out.println("Divisão [/] ");
+            String op = entrada.next();
+
+            switch (op) {
+                case "+":
+                    cal.somar();
+                    break;
+                case "-":
+                    cal.subtrair();
+                    break;
+                case "*":
+                    cal.multiplicar();
+                    break;
+                case "/":
+                    cal.dividir();
+                    break;
+                default:
+                    System.out.println("Operação inválida.");
+            }
+
+            entrada.close();
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
 }
